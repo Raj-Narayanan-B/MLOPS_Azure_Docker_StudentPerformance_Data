1c57762f65d5044415bcd151e845d81ef7fdf7d3
Raj Narayanan <brajnarayanan.b@gmail.com>
2023-10-12 22:35:26 +0530

    exception and logging created

--- template.py
+++ template.py
@@ -17,7 +17,20 @@ files = [
     "setup.py",
     "requirements.txt",
     "README.md",
-    os.path.join("src","__init__.py")
+    
+    os.path.join("src","__init__.py"),
+    os.path.join("src","exception.py"),
+    os.path.join("src","logger.py"),
+    os.path.join("src","utils.py"),
+
+    os.path.join("src","components","__init__.py"),
+    os.path.join("src","components","data_ingestion.py"),
+    os.path.join("src","components","data_transformer.py"),
+    os.path.join("src","components","model_trainer.py"),
+
+    os.path.join("src","pipeline","__init__.py"),
+    os.path.join("src","pipeline","predict_pipeline.py"),
+    os.path.join("src","pipeline","train_pipeline.py")
 ]
 
 for i in files:
