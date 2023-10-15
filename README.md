c40a55bed190bdb006c4c6b5053d1fd6b80e8f2c
Raj Narayanan <brajnarayanan.b@gmail.com>
2023-10-15 10:40:42 +0530

    data_ingestion added, notebooks added, params.yaml added

---
+++ README.md
@@ -0,0 +1,4 @@
+conda create -p student_performance python=3.11.5 -y
+
+Create a conda environment named student_performance with python version 3.11.5 
+Here, -p will specify the terminal to create the environment to be in the pwd
---
+++ src/components/data_ingestion.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import pandas as pd
+from src.logger import logging
+from src.exception import CustomException
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class data_path:
+    train_data_path = os.path.join("artifacts","train.csv")
+    test_data_path = os.path.join("artifacts","test.csv")
+    
+class data_ingestion:
+    def __init__(self):
+        self.train_path = data_path.train_data_path
+        self.test_path = data_path.test_data_path
+        logging.info("Train-Test paths created")
+
+    def data_load(self):
+        try:    
+            data_path = os.path.join("artifacts","data.csv")
+            df = pd.read_csv(data_path)
+            logging.info("Data loaded")
+
+            train_data,test_data = train_test_split(df,test_size=.25,random_state=8)
+            logging.info("Train_data, Test_data split successful")
+
+            train_data.to_csv(self.train_path,index=False)
+            test_data.to_csv(self.test_path,index=False)
+            logging.info("Train_data, Test_data loaded")
+
+            return (train_data,test_data)
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+if __name__=="__main__":
+    obj=data_ingestion()
+    train_data,test_data = obj.data_load()
