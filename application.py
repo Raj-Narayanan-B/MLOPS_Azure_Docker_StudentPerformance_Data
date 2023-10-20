9c1298d10063512ed7d103ae29f8171b8a85097f
Raj Narayanan B <brajnarayanan.b@gmail.com>
2023-10-20 09:03:53 +0530

    Update application.py

--- application.py
+++ application.py
@@ -28,4 +28,4 @@ def predict():
         return render_template('home.html', results = result)
     
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=80)
